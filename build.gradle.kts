560a7007b448254b41d6adb65ea8da533d872bac
Oliver Berg <oliver@berg.moe>
2022-07-28 17:03:41 +0200

    Migrate from Groovy build-scripts to Kotlin build-scripts
    The Kotlin version has also been updated from 1.0.6 to 1.7.10, and the publishing block has been removed for now.

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "azadev.kotlin"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.9")
+}
