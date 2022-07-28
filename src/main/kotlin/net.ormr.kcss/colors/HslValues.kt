d73f8a266afaa261e1649016de2004cac336b54f
Oliver Berg <oliver@berg.moe>
2022-07-28 18:40:56 +0200

    Introduce a hierarchy to `Color`

--- src/main/kotlin/net.ormr.kcss/colors/HslValues.kt
+++ src/main/kotlin/net.ormr.kcss/colors/HslValues.kt
@@ -1,17 +1,15 @@
 package net.ormr.kcss.colors
 
 class HslValues(
-	var hue: Float,
-	var saturation: Float,
-	var lightness: Float,
+    var hue: Float,
+    var saturation: Float,
+    var lightness: Float,
 ) {
-    fun setLightnessSafe(l: Float): HslValues {
-        lightness = Math.min(1f, Math.max(0f, l))
-        return this
+    fun setLightnessSafe(l: Float): HslValues = apply {
+        lightness = 1F.coerceAtMost(0F.coerceAtLeast(l))
     }
 
-    fun setSaturationSafe(l: Float): HslValues {
-        saturation = Math.min(1f, Math.max(0f, l))
-        return this
+    fun setSaturationSafe(l: Float): HslValues = apply {
+        saturation = 1F.coerceAtMost(0F.coerceAtLeast(l))
     }
 }
--- src/main/kotlin/net.ormr.kcss/colors/colors.kt
+++ src/main/kotlin/net.ormr.kcss/colors/colors.kt
@@ -1,9 +1,9 @@
 package net.ormr.kcss.colors
 
-fun hex(hex: String): Color = Color.fromHex(hex)
+fun hex(hex: String): CustomColor = CustomColor.fromHex(hex)
 
-fun hex(hex: Int): Color = Color.fromHex(hex)
+fun hex(hex: Int): CustomColor = CustomColor.fromHex(hex)
 
-fun rgb(r: Int, g: Int, b: Int): Color = Color.fromRgb(r, g, b)
+fun rgb(r: Int, g: Int, b: Int): CustomColor = CustomColor.fromRgb(r, g, b)
 
-fun rgba(r: Int, g: Int, b: Int, a: Number = 1F): Color = Color.fromRgb(r, g, b, a.toFloat())
+fun rgba(r: Int, g: Int, b: Int, a: Number = 1F): CustomColor = CustomColor.fromRgb(r, g, b, a.toFloat())
